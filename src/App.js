be0734fcfbff048812534321077f835bd0beca9c
Caleb <caleb@calebcurry.com>
2022-09-18 18:26:31 -0400

    graphql testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+import { useEffect } from 'react';
+
+const GET_DATA = gql`
+    {
+        launchesPast(limit: 10) {
+            mission_name
+            launch_date_local
+            launch_site {
+                site_name_long
+            }
+            links {
+                article_link
+                video_link
+            }
+            rocket {
+                rocket_name
+            }
+        }
+    }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { loading, error, data } = useQuery(GET_DATA);
+    useEffect(() => {
+        console.log(loading, error, data);
+    }, [loading, error, data]);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error</p>;
+
+    return data
+        ? data.launchesPast.map((launch) => <h3>{launch.mission_name}</h3>)
+        : null;
 }
 
 export default App;
